3f5f278cb3ab939a4b369222ba2d1599dea41f6c
HumanBojack <spychalar59@gmail.com>
2021-04-21 16:15:13 +0200

    Map for bike station

--- velibs/script.js
+++ velibs/script.js
@@ -1,4 +1,3 @@
-
 const fetchApi = () => {
 	return fetch("https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=&rows=100&refine.name=Gare+du+Nord+-+Dunkerque")
 		.then(data => data.json());
@@ -15,8 +14,15 @@ async function updatePage(){
 			<li>${station.name}</li>
 			<li>${station.ebike} ebikes available</li>
 			<li>${station.mechanical} regular bikes available</li>
-			<li>Location: ${station.coordonnees_geo[0]} ${station.coordonnees_geo[1]}</li>
+			<div id="map"></div>
 		</ul>`
+
+		let map = L.map('map').setView(station.coordonnees_geo, 18);
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+		    maxZoom: 18
+		}).addTo(map);
+		L.marker(station.coordonnees_geo).addTo(map);
 }
 
 updatePage()
